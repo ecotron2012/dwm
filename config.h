3f4b51e6088e05e3146c34cfc3ea793466d38ee5
Cristobal Carreño <cristblcarreo.f@gmail.com>
2026-06-09 12:17:43 -0400

    update config

--- config.h
+++ config.h
@@ -124,6 +124,14 @@ static const char *projectscreen[] = { "sh", "-c",
 	"xrandr --output eDP --mode 1920x1080 --output DisplayPort-0 --mode 1920x1080 --same-as eDP",
 	NULL };
 
+/* open default web browser */
+static const char *openwebbrowser[] = {"xdg-open", "http://",
+	NULL };
+
+/* access passwords with passmenu */
+static const char *openpassmenu[] = {"passmenu",
+	NULL };
+
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
@@ -174,6 +182,8 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask, 		XK_s,       spawn, 	   {.v = screencap} },
 	{ MODKEY|ShiftMask, 		XK_p,       spawn, 	   {.v = scrcapcurrentwindow} },
 	{ MODKEY, 			XK_End,       spawn, 	   {.v = screenlock} },
+	{ MODKEY|ShiftMask, 		XK_b,	      spawn, 	   {.v = openwebbrowser}},
+	{ MODKEY|ShiftMask, 		XK_k,	      spawn, 	   {.v = openpassmenu}},
 };
 
 /* button definitions */
