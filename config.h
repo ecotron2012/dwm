e6e09ebd6972f70b36c6b089cffb72df96cf95ba
Cristóbal Carreño <cristblcarreo.f@gmail.com>
2026-06-19 12:25:32 -0400

    fix status bar not hiding with togglebar func

--- config.h
+++ config.h
@@ -115,8 +115,8 @@ static const char *scrcapcurrentwindow[] = { "sh", "-c",
 	"maim -i $(xdotool getactivewindow) ~/screenshots/screenshot_$(date --iso-8601='seconds').jpg",
 	NULL };
 
-/* screen lock with xsecurelock */
-static const char *screenlock[] = { "xsecurelock",
+/* screen lock with xscreensaver */
+static const char *screenlock[] = { "xscreensaver-command", "-lock",
 	NULL };
 
 /* screen projection with xrandr */
@@ -129,7 +129,7 @@ static const char *openwebbrowser[] = {"xdg-open", "http://",
 	NULL };
 
 /* access passwords with passmenu */
-static const char *openpassmenu[] = {"passmenu",
+static const char *openpassmenu[] = {"passmenu", "-fn", dmenufont,
 	NULL };
 
 static const Key keys[] = {
--- dwm.c
+++ dwm.c
@@ -2043,6 +2043,7 @@ togglebar(const Arg *arg)
 		}
 		XConfigureWindow(dpy, systray->win, CWY, &wc);
 	}
+ 	XMoveResizeWindow(dpy, selmon->barwin, selmon->wx, selmon->by, selmon->ww, bh);
 	arrange(selmon);
 }
 
