5484fcaeb60eab04ada085a7d17fce1287ef420d
Cristóbal Carreño <cristblcarreo.f@gmail.com>
2026-08-13 10:55:12 -0400

    add neomutt shortcut

--- config.h
+++ config.h
@@ -14,8 +14,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Symbols Nerd Font:size=12:antialias=true:autohint=true", "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "Symbols Nerd Font:size=12:antialias=true:autohint=true", "Hack Nerd Font Mono:size=10" };
+static const char dmenufont[]       = "Hack Nerd Font Mono:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -180,6 +180,7 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask, 		XK_k,	      spawn, 	   {.v = openpassmenu}},
 	{ MODKEY|ShiftMask, 		XK_m,	      spawn, 	   SHCMD("xdotool type $(grep -v '^#' ~/.config/bookmarks.txt | dmenu -i -l 50 | cut -d' ' -f1)")},
 	{ MODKEY|ShiftMask, 		XK_t,	      spawn, 	   SHCMD("projectscreen")},
+	{ MODKEY | ShiftMask, XK_c, spawn, SHCMD("st -t neomutt -e bash -ic neomutt")},
 };
 
 /* button definitions */
