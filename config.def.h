34d670658cabf9189adc39ee3f89ad37c59dc88d
Cristóbal Carreño <cristblcarreo.f@gmail.com>
2026-08-26 11:18:38 -0400

    fix bugs with xresources patch

--- config.def.h
+++ config.def.h
@@ -2,9 +2,9 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx = 1; /* border pixel of windows */
-static const unsigned int gappx = 5;    /* gaps between windows */
-static const unsigned int snap = 32;    /* snap pixel */
+static unsigned int borderpx = 1;    /* border pixel of windows */
+static const unsigned int gappx = 5; /* gaps between windows */
+static unsigned int snap = 32;       /* snap pixel */
 static const int swallowfloating =
     0; /* 1 means swallow floating windows by default */
 static const unsigned int systraypinning =
@@ -15,8 +15,8 @@ static const int systraypinningfailfirst =
     1; /* 1: if pinning fails, display systray on the first monitor, False:
           display systray on the last monitor*/
 static const int showsystray = 1; /* 0 means no systray */
-static const int showbar = 1;     /* 0 means no bar */
-static const int topbar = 1;      /* 0 means bottom bar */
+static int showbar = 1;           /* 0 means no bar */
+static int topbar = 1;            /* 0 means bottom bar */
 static const char *fonts[] = {"monospace:size=10"};
 static const char dmenufont[] = "monospace:size=10";
 static const char col_gray1[] = "#222222";
--- config.h
+++ config.h
@@ -2,9 +2,9 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx = 3; /* border pixel of windows */
-static const unsigned int gappx = 5;    /* gaps between windows */
-static const unsigned int snap = 32;    /* snap pixel */
+static unsigned int borderpx = 3;    /* border pixel of windows */
+static const unsigned int gappx = 5; /* gaps between windows */
+static unsigned int snap = 32;       /* snap pixel */
 static const int swallowfloating =
     0; /* 1 means swallow floating windows by default */
 static const unsigned int systraypinning =
@@ -15,8 +15,8 @@ static const int systraypinningfailfirst =
     1; /* 1: if pinning fails, display systray on the first monitor, False:
           display systray on the last monitor*/
 static const int showsystray = 1; /* 0 means no systray */
-static const int showbar = 1;     /* 0 means no bar */
-static const int topbar = 1;      /* 0 means bottom bar */
+static int showbar = 1;           /* 0 means no bar */
+static int topbar = 1;            /* 0 means bottom bar */
 static const char *fonts[] = {
     "Symbols Nerd Font:size=12:antialias=true:autohint=true",
     "Hack Nerd Font Mono:size=10"};
@@ -91,10 +91,9 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact = 0.55; /* factor of master area size [0.05..0.95] */
-static const int nmaster = 1;    /* number of clients in master area */
-static const int resizehints =
-    1; /* 1 means respect size hints in tiled resizals */
+static float mfact = 0.55;  /* factor of master area size [0.05..0.95] */
+static int nmaster = 1;     /* number of clients in master area */
+static int resizehints = 1; /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen =
     1; /* 1 will force focus on the fullscreen window */
 static const int refreshrate =
@@ -131,7 +130,8 @@ static char dmenumon[2] =
 static char *dmenucmd[] = {
     "dmenu_run", "-m",      dmenumon,  "-fn",     "Hack Nerd Font Mono:size=10",
     "-nb",       "#222222", "-nf",     "#bbbbbb", "-sb",
-    "#005577",   "-sf",     "#eeeeee", NULL};
+    "#005577",   "-sf",     "#eeeeee", "-ob",     "#005577",
+    NULL};
 static const char *termcmd[] = {"st", NULL};
 
 /* brightness control */
@@ -233,6 +233,7 @@ static const XResPref resources[] = {
     {"dwm.foreground", STRING, &dmenucmd[8]},
     {"dwm.backgroundSel", STRING, &dmenucmd[10]},
     {"dwm.foregroundSel", STRING, &dmenucmd[12]},
+    {"dwm.backgroundSel", STRING, &dmenucmd[14]},
     {"dwm.foreground", STRING, &colors[SchemeNorm][ColFg]},
     {"dwm.background", STRING, &colors[SchemeNorm][ColBg]},
     {"dwm.border", STRING, &colors[SchemeNorm][ColBorder]},
